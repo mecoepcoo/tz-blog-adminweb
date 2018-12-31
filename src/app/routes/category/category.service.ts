c00d978aa9d7efd3ec4ee077f30f308a238e3ff1
tianzhen <340349237@qq.com>
2018-12-31 23:42:19 +0800

    增加接口结构

---
+++ src/app/routes/category/category.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class CategoryService {
+  constructor() {
+    
+  }
+
+  /* 获取分类列表 */
+  getCategoryList(page = 1, size = 10) {
+    
+  }
+}
