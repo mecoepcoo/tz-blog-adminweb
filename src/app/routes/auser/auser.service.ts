8f9013dffc006e7fe25a6ab8c079a78f7507906b
Tianzhen <340349237@qq.com>
2019-01-07 23:27:10 +0800

    增加管理员服务

--- src/app/routes/auser/auser.service.ts
+++ src/app/routes/auser/auser.service.ts
@@ -1,47 +1,47 @@
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { ICategory } from '@interfaces/category';
+import { IAuser } from '@interfaces/auser';
 import { IResponse } from '@interfaces/response';
 import { Config } from '@config/config';
 
 @Injectable()
-export class CategoryService {
+export class AuserService {
   constructor(
     private http: HttpClient,
   ) { }
 
-  /* 获取分类列表 */
-  getCategoryList(page = 1, size = 10): Observable<IResponse> {
-    let url = `${Config.apiUrl}categories?page=${page}&size=${size}`;
+  /* 获取管理员列表 */
+  getAuserList(page = 1, size = 10): Observable<IResponse> {
+    let url = `${Config.apiUrl}ausers?page=${page}&size=${size}`;
     return this.http.get(url);
   }
 
-  /* 获取分类 */
-  getCategory(id: number | string): Observable<IResponse> {
-    let url = `${Config.apiUrl}categories/${id}`;
+  /* 获取管理员分类 */
+  getAuser(id: number | string): Observable<IResponse> {
+    let url = `${Config.apiUrl}ausers/${id}`;
     return this.http.get(url);
   }
 
-  /* 新增分类 */
-  addCategory(name): Observable<IResponse> {
-    let url = `${Config.apiUrl}categories`;
+  /* 新增管理员 */
+  addAuser(name): Observable<IResponse> {
+    let url = `${Config.apiUrl}ausers`;
     return this.http.post(url, {
       name: name,
     });
   }
 
-  /* 修改分类 */
-  editCategory(id, name): Observable<IResponse> {
-    let url = `${Config.apiUrl}categories/${id}`;
+  /* 修改管理员 */
+  editAuser(id, name): Observable<IResponse> {
+    let url = `${Config.apiUrl}ausers/${id}`;
     return this.http.put(url, {
       name: name,
     });
   }
 
-  /* 删除分类 */
-  delCategory(id): Observable<IResponse> {
-    let url = `${Config.apiUrl}categories/${id}`;
+  /* 删除管理员 */
+  delAuser(id): Observable<IResponse> {
+    let url = `${Config.apiUrl}ausers/${id}`;
     return this.http.delete(url);
   }
 }
